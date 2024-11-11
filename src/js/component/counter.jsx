d5bde26af40c3685372ba079ef0cac1116fcfe53
Erik Martin <erikmartinfernandez@gmail.com>
2024-11-11 13:55:31 +0000

    Entrega buena

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -1,58 +1,74 @@
+//Imports
 import React, { useEffect, useState } from 'react';
 
+
+
 const Timer = () => {
-    const [time, setTime] = useState({
-        hours: 0,
-        minutes: 0,
-        seconds: 0
-    });
 
-// PrevTime lo usamos para saber el estado anterior del componente y poder actualizarlo con +1 cuando llegue al volr deseado por ejemplo cada 60 segundos añadimos +1 a los minutos
+  const [time, setTime] = useState({
+    fifth: 9,
+    fourth: 9,
+    third: 9,
+    second: 9,
+    first:0
+  });
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTime(prevTime => {
-                let { hours, minutes, seconds } = prevTime;
+  // PrevTime lo usamos para saber el estado anterior del componente y poder actualizarlo con +1 cuando llegue al volr deseado por ejemplo cada 60 segundos añadimos +1 a los minutos
 
-                seconds += 1;
-                if (seconds === 60) {
-                    seconds = 0;
-                    minutes += 1;
-                }
-                if (minutes === 60) {
-                    minutes = 0;
-                    hours += 1;
-                }
-                if (hours === 24) {
-                    hours = 0;
-                }
+useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(prevTime => {
+        let { fifth, fourth, third, second, first,  } = prevTime;
 
-                return { hours, minutes, seconds };
-            });
-        }, 1000);
+        first += 1;
+        if (first === 10) {
+          first = 0;
+          second += 1;
+        }
+        if (second === 10) {
+          second = 0;
+          third += 1;
+        }
+        if (third === 10) {
+          third = 0;
+          fourth += 1;
+        }
+        if (fourth === 10) {
+          fourth = 0;
+          fifth += 1;
+        }
+        if (fifth === 10) {
+          fifth = 0;
+       }
 
-        // el ,1000 al final es para ejecutarlo cada 1 segundo
+
+        return { first, second, third,fourth,fifth };
+      });
+    }, 1000);
+// el ,1000 al final es para ejecutarlo cada 1 segundo
 
 
-        return () => clearInterval(interval);
-    }, []);
+return () => clearInterval(interval);
+  }, []);
 
-    
-    // El padStart sirve para poner digitos de 2 numeros (y empezamos en 0)
-    const Times = (unit) => {
-      return unit.toString().padStart(2, '0');
+
+  // El padStart sirve para poner digitos de 1,2,3... unidades etz (en este caso 1)
+  const Times = (time) => {
+    return time.toString().padStart(1, '0');
+
   };
 
-    return (
-        <div className="counter px-5 font-family-monospace">
-            <div className="icon border-bottom border-white px-2 font-monospace"><i class="fa-solid fa-clock"></i></div>
-            <div className="hour border-bottom border-white px-2 font-monospace">{Times(time.hours)}</div>
-            <div className="separator border-bottom border-white font-monospace">:</div>
-            <div className="minute border-bottom border-white px-2 font-monospace">{Times(time.minutes)}</div>
-            <div className="separator border-bottom border-white font-monospace">:</div>
-            <div className="second border-bottom border-white px-2 font-monospace">{Times(time.seconds)}</div>
-        </div>
-    );
+    
+  return (
+    <div className="counter px-5 font-family-monospace">
+      <div className="icon border-bottom border-white px-2 font-monospace"><i class="fa-solid fa-clock"></i></div>
+      <div className="hour border-bottom border-white px-2 font-monospace">{Times(time.fifth)}</div>
+      <div className="minute border-bottom border-white px-2 font-monospace">{Times(time.fourth)}</div>
+      <div className="second border-bottom border-white px-2 font-monospace">{Times(time.third)}</div>
+      <div className="second border-bottom border-white px-2 font-monospace">{Times(time.second)}</div>
+      <div className="second border-bottom border-white px-2 font-monospace">{Times(time.first)}</div>
+    </div>
+  );
 };
 
 export default Timer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,13 @@
+//imports
+
 import React from "react";
 import Timer from "./counter";
 
-//include images into your bundle
 
-
-//create your first component
 const Home = () => {
 	return (
 		<>
 		<Timer />
-		
-		
-		
-		
 		</>
 	);
 };
